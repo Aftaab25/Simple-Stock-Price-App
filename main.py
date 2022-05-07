e11cfbee70a7432c63bdae375302a81e9b11bdf1
Aftaab Siddiqui <aftaab2507@gmail.com>
2022-05-08 01:23:28 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -15,5 +15,14 @@ tickerData = yf.Ticker(tickerSymbol)
 
 tickerDf = tickerData.history(period='id', start='2010-5-31', end='2020-5-31')
 
+st.write("""
+### Closing Price
+""")
+
 st.line_chart(tickerDf.Close)
+
+st.write("""
+### Volume
+""")
+
 st.line_chart(tickerDf.Volume)
